6d183f690989bf391b84bc06eee3b40c93055248
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-07-04 18:02:52 +0600

    clean

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,8 +2,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { project } from './project'
 import { skill } from './skill'
-import { message } from './message' // নতুন ইমপোর্ট
+import { message } from './message'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [project, skill, message], // এখানে যুক্ত হলো
+  types: [project, skill, message],
 }
